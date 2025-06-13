08012d24d2fd271feccfceae96c5f18ebf251b75
Greg Larkin <gclhub@github.com>
2025-06-13 16:39:16 -0700

    Bump version to 1.0.1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version is the current version of the extension
-	Version = "1.0.0"
+	Version = "1.0.1"
 
 	// CommitHash is the git commit hash at build time
 	CommitHash = "unknown"
